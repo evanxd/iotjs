b5e5e25b76dbb903808d7afb798ad8ba406c76b2
Evan Tseng <evan@tseng.io>
2016-07-30 22:13:04 +0800

    Add thingjs shell command

---
+++ thingjs.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var path = require('path');
+var util = require('./lib/util');
+var parentDir = path.dirname(module.parent.filename);
+var board = require(parentDir + '/board.json');
+var thingjs = {};
+
+for (var key in board.modules) {
+  var Module = require('./lib/' + util.module2fileName(board.modules[key].module));
+  thingjs[key] = new Module(board.modules[key].pins);
+}
+
+for (key in board.extensions) {
+  var extension = require(key);
+  var params = board.extensions[key];
+  for (key in params) {
+    if (key === 'modules') {
+      params[key] = thingjs[params[key]];
+    }
+  }
+  extension.run(params);
+}
