4869e70a182123ab03aadb4d32272ace4d920dc9
Evan Tseng <evan@tseng.io>
2016-07-31 00:36:49 +0800

    Add handleBoardJson method in util module

--- index.js
+++ index.js
@@ -4,22 +4,6 @@ var path = require('path');
 var util = require('./lib/util');
 var parentDir = path.dirname(module.parent.filename);
 var board = require(parentDir + '/board.json');
-var thingjs = {};
-
-for (var key in board.modules) {
-  var Module = require('./lib/' + util.module2fileName(board.modules[key].module));
-  thingjs[key] = new Module(board.modules[key].pins);
-}
-
-for (key in board.extensions) {
-  var extension = require(key);
-  var params = board.extensions[key];
-  for (key in params) {
-    if (key === 'modules') {
-      params[key] = thingjs[params[key]];
-    }
-  }
-  extension.run(params);
-}
+var thingjs = util.handleBoardJson(board);
 
 module.exports = thingjs;
