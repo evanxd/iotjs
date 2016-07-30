6c0dc4d67d9054e7a1f93b2f34c3197b34a2a50b
Evan Tseng <evan@tseng.io>
2016-07-30 21:24:23 +0800

    Add util module

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 'use strict';
 
 var path = require('path');
+var util = require('./lib/util');
 var parentDir = path.dirname(module.parent.filename);
 var board = require(parentDir + '/board.json');
 var thingjs = {};
 
 for (var id in board.modules) {
-  var Module = require(module2filePath(board.modules[id].module));
+  var Module = require('./lib/' + module2fileName(board.modules[id].module));
   thingjs[id] = new Module(board.modules[id].pins);
-}
-
-function module2filePath (moduleName) {
-  var fileName = '';
-  for (var i = 0; i < moduleName.length; i++) {
-    var character = moduleName.charAt(i);
-    if (character === character.toLowerCase()) {
-      fileName += character;
-    } else {
-      if (fileName === '') {
-        fileName += character.toLowerCase();
-      } else {
-        fileName += '-' + character.toLowerCase();
-      }
-    }
-  }
-  return './lib/' + fileName;
 }
 
 module.exports = thingjs;
