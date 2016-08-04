773e5b2fb2ababa7d4ba64de8856cbcfe125ed13
Evan Tseng <evan@tseng.io>
2016-08-04 14:59:11 +0800

    Fix bugs

--- extension/rest-server/index.js
+++ extension/rest-server/index.js
@@ -5,30 +5,30 @@ var express = require('express');
 module.exports = {
   /**
    * The run method is the start point of the extension.
-   * @param {Object}       params         The extension parameters.
-   * @param {Object|Array} params.modules The module instances.
+   * @param {Object} params         The extension parameters.
+   * @param {Array}  params.modules The module instances.
    */
   run: function(params) {
     var app = express();
-    var modules = params.modules;
-    modules = Array.isArray(modules) ? modules : [modules];
 
     modules.forEach(function(_module) {
       app.get('/modules/' + _module.id, function (req, res) {
-        var action = req.query.action;
-        var moduleData = {};
         if (_module.type === 'input') {
+          var moduleData = {};
           _module.on('data', function(data) {
             moduleData = data;
           });
           res.jsonp({ result: 'success', module: _module.module, data: moduleData });
         } else if (_module.type === 'output') {
+          var action = req.query.action;
           if (_module[action] && typeof _module[action] === 'function') {
             _module[action].apply(_module, [req.query.params]);
             res.jsonp({ result: 'success', action: action });
           } else {
             res.jsonp({ result: 'failure', message: 'No such action.' });
           }
+        } else {
+          console.warn('No such module type.');
         }
       });
     });
