ef76dacf5727fcc927f9b8adf4895986419c4b21
Evan Tseng <evan@tseng.io>
2016-08-04 14:50:29 +0800

    Support multiple modules in an extension

--- extension/rest-server/index.js
+++ extension/rest-server/index.js
@@ -5,29 +5,32 @@ var express = require('express');
 module.exports = {
   /**
    * The run method is the start point of the extension.
-   * @param {Object} params         The extension parameters.
-   * @param {Object} params.modules The module instance.
+   * @param {Object}       params         The extension parameters.
+   * @param {Object|Array} params.modules The module instances.
    */
   run: function(params) {
     var app = express();
     var modules = params.modules;
+    modules = Array.isArray(modules) ? modules : [modules];
 
-    app.get('/modules/' + modules.id, function (req, res) {
-      var action = req.query.action;
-      var moduleData = {};
-      if (modules.type === 'input') {
-        modules.on('data', function(data) {
-          moduleData = data;
-        });
-        res.jsonp({ result: 'success', module: modules.module, data: moduleData });
-      } else if (modules.type === 'output') {
-        if (modules[action] && typeof modules[action] === 'function') {
-          modules[action].apply(modules, [req.query.params]);
-          res.jsonp({ result: 'success', action: action });
-        } else {
-          res.jsonp({ result: 'failure', message: 'No such action.' });
+    modules.forEach(function(_module) {
+      app.get('/modules/' + _module.id, function (req, res) {
+        var action = req.query.action;
+        var moduleData = {};
+        if (_module.type === 'input') {
+          _module.on('data', function(data) {
+            moduleData = data;
+          });
+          res.jsonp({ result: 'success', module: _module.module, data: moduleData });
+        } else if (_module.type === 'output') {
+          if (_module[action] && typeof _module[action] === 'function') {
+            _module[action].apply(_module, [req.query.params]);
+            res.jsonp({ result: 'success', action: action });
+          } else {
+            res.jsonp({ result: 'failure', message: 'No such action.' });
+          }
         }
-      }
+      });
     });
 
     app.listen(3000, function () {
