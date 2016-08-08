cb2e3560d6d2e791c53b3e1272481b4b9a45459c
Evan Tseng <evan@tseng.io>
2016-08-08 22:22:55 +0800

    Export module lib

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var path = require('path');
 var util = require('./lib/util');
 var parentDir = path.dirname(module.parent.filename);
 var board = require(parentDir + '/board.json');
-var thingjs = util.handleBoardJson(board);
 
-module.exports = thingjs;
+module.exports = {
+  modules:    util.getModules(board),
+  DustSensor: require('./module/dust-sensor'),
+  Led:        require('./module/led'),
+  Switch:     require('./module/switch')
+};
