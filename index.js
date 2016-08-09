f06795e8ffb810ea8686ef5cd44b3c831c72a3ea
Evan Tseng <evan@tseng.io>
2016-08-09 11:05:21 +0800

    Add board.json parser object

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var path = require('path');
-var util = require('./lib/util');
+var parser = require('./lib/board-parser');
 var parentDir = path.dirname(module.parent.filename);
 var board = require(parentDir + '/board.json');
 
 module.exports = {
-  modules:    util.getModules(board),
+  modules:    parser.getModules(board),
   DustSensor: require('./module/dust-sensor'),
   Led:        require('./module/led'),
   Switch:     require('./module/switch')
