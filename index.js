0be7dca78af6dd2bcc4b6e9ffebfe8ae16484fc9
Evan Tseng <evan@tseng.io>
2016-08-09 11:50:34 +0800

    Refactor BoardParser object: add parse method

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var path = require('path');
 var parser = require('./lib/board-parser');
+var path = require('path');
 var parentDir = path.dirname(module.parent.filename);
 var board = require(parentDir + '/board.json');
+var modules = parser.parse(board);
 
 module.exports = {
-  modules:    parser.getModules(board),
+  modules:    modules,
   DustSensor: require('./module/dust-sensor'),
   Led:        require('./module/led'),
   Switch:     require('./module/switch')
