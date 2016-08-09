e52cd87f3ffaf6f4df308587b86bd03010193eef
Evan Tseng <evan@tseng.io>
2016-08-09 12:02:40 +0800

    Update comments

--- extension/thingjs-blink/index.js
+++ extension/thingjs-blink/index.js
@@ -3,9 +3,9 @@
 module.exports = {
   /**
    * The run method is the start point of the extension.
-   * @param {Object}    params          The extension parameters.
-   * @param {Led|Array} params.modules  The LED instances.
-   * @param {Number}    params.interval The blinking rate.
+   * @param {Object} params          The extension parameters.
+   * @param {Array}  params.modules  The LED instances.
+   * @param {Number} params.interval The blinking rate.
    */
   run: function(params) {
     params.modules.forEach(function(_module) {
