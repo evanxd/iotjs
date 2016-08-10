e599bced5a44093cc49029120c5e4d85f4210101
Evan Tseng <evan@tseng.io>
2016-08-10 19:53:16 +0800

    Implement RGB LED module

--- module/rgb-led.js
+++ module/rgb-led.js
@@ -1,36 +1,103 @@
 'use strict';
 
+var IO = require('thingjs-io');
+
 /**
  * RGB LED module.
  * @constructor
  * @param {Array} pins The pin numbers.
  */
 function RgbLed (pins) {
-  this.pins = pins;
+  this._red =   new IO(pins[0], 'pwm');
+  this._green = new IO(pins[1], 'pwm');
+  this._blue =  new IO(pins[2], 'pwm');
 }
 
+RgbLed.DEFAULT_INTERVAL = 500;
+RgbLed.DEFAULT_PERIOD = 19200000;
+
 RgbLed.prototype = {
-  id: null,
-  module: 'RgbLed',
-  type: 'output',
+  id:       null,
+  module:   'RgbLed',
+  type:     'output',
+  color:    null,
+  _red:     null,
+  _green:   null,
+  _blue:    null,
+  _timerId: 0,
 
   /**
-   * Light on the RGB LED.
-   * @param {String} color Color code or name.
+   * Turn on the RGB LED.
+   * @param {String} [color] Color code or name, e.g. #28ABE3.
    */
-  on: function (color) {},
+  on: function (color) {
+    this._timerId && this.stopBlink();
+    if (color) {
+      this.color = color;
+      var dutyCycles = this._color2dutyCycles(color);
+      this._red.val(dutyCycles[0], RgbLed.DEFAULT_PERIOD);
+      this._green.val(dutyCycles[1], RgbLed.DEFAULT_PERIOD);
+      this._blue.val(dutyCycles[2], RgbLed.DEFAULT_PERIOD);
+    }
+    this._red.enable();
+    this._green.enable();
+    this._blue.enable();
+  },
 
   /**
-   * Light off the RGB LED.
+   * Turn off the RGB LED.
    */
-  off: function () {},
+  off: function () {
+    this._timerId && this.stopBlink();
+    this._red.disable();
+    this._green.disable();
+    this._blue.disable();
+  },
 
   /**
    * Blink the RGB LED.
-   * @param {String} color Color code or name.
-   * @param {Number} interval The interval of blinking the RGB LED.
+   * @param {Number} [interval] The interval of blinking the RGB LED.
+   * @param {String} [color] Color code or name.
    */
-  blink: function (color, interval) {}
+  blink: function (interval, color) {
+    interval = interval || RgbLed.DEFAULT_INTERVAL;
+    if (color) {
+      this.on(color);
+      this.color = color;
+    }
+    var isEnable = true;
+    this._timerId = setInterval(function() {
+      isEnable ? this.on() : this.off();
+      isEnable = !isEnable;
+    }, interval);
+  },
+
+  /**
+   * Stop blinking the RGB LED.
+   */
+  stopBlink: function() {
+    clearInterval(this._timerId);
+    this._timerId = 0;
+  },
+
+  /**
+   * Get duty cycle values of the red, green, and blue LEDs.
+   * @param {String} color Color code or name, e.g. #28ABE3.
+   * @return {Array} Duty cycle values.
+   */
+  _color2dutyCycles: function(color) {
+    var dutyCycles = [0, 0, 0];
+    if (color.charAt(0) === '#' && color.length === 7) {
+      for (var i = 0; i < 3; i++) {
+        var dutyCycle = RgbLed.DEFAULT_PERIOD *
+          parseInt(color.substr(1 + 2 * i, 2), 16) / 255;
+        dutyCycles[i] = dutyCycle;
+      }
+    } else {
+      console.warn('The color is invalid.');
+    }
+    return dutyCycles;
+  }
 };
 
 module.exports = RgbLed;
